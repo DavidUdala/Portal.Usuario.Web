07688f2556c8cad5431529c62178bf84440702aa
David <david_udala@hotmail.com>
2025-10-31 14:21:56 -0300

    feat: adicionado interceptor

--- src/app/pages/usersignup/usersignup.component.ts
+++ src/app/pages/usersignup/usersignup.component.ts
@@ -5,10 +5,13 @@ import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 @Component({
   selector: 'app-usersignup',
+  providers: [provideNativeDateAdapter()],
   imports: [FormsModule, MatFormFieldModule, MatInputModule,
-            MatButtonModule, MatDividerModule, MatIconModule],
+            MatButtonModule, MatDividerModule, MatIconModule, MatDatepickerModule],
   templateUrl: './usersignup.component.html',
   styleUrl: './usersignup.component.css'
 })
