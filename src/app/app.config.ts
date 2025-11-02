6ebb663e5caf584b96230bd423cc2a9aab35f9be
David <david_udala@hotmail.com>
2025-11-02 00:21:30 -0300

    feat: adicionado valicao do formulario de usuario e adicionado provide de pt-bt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true })
-    ,provideRouter(routes)
-    ,provideClientHydration(withEventReplay())
-    ,provideHttpClient(
+    , provideRouter(routes)
+    , provideClientHydration(withEventReplay())
+    , provideHttpClient(
       withInterceptors([authInterceptor])
-    )]
+    ),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: DateAdapter, useClass: NativeDateAdapter, deps: [MAT_DATE_LOCALE] },
+  ]
 };
--- src/app/pages/usersignup/usersignup.component.ts
+++ src/app/pages/usersignup/usersignup.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,25 +9,41 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { UserService } from '../../services/user.service';
 import { PostUserInput } from '../../interfaces/postUserInput';
-import { pipe } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-usersignup',
   providers: [provideNativeDateAdapter()],
   imports: [FormsModule, MatFormFieldModule, MatInputModule,
-    MatButtonModule, MatDividerModule, MatIconModule, MatDatepickerModule],
+    MatButtonModule, MatDividerModule, MatIconModule, MatDatepickerModule,
+    ReactiveFormsModule, CommonModule],
   templateUrl: './usersignup.component.html',
   styleUrl: './usersignup.component.css'
 })
-export class UsersignupComponent {
+export class UsersignupComponent implements OnInit {
   constructor(private userService: UserService) { }
 
-  user : PostUserInput = new PostUserInput();
+  user: PostUserInput = new PostUserInput();
+  userForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      birthDay: new FormControl('', [Validators.required, Validators.maxLength(10)])
+    })
+  }
 
   register() {
-     this.userService.create(this.user)
-     .subscribe(resp => {
-        if(resp.success)
-          console.log(resp.message);
-     })
+    if (this.userForm.valid) {
+      this.user = this.userForm.value as PostUserInput;
+
+      this.userService.create(this.user)
+        .subscribe(resp => {
+          if (resp.success)
+            console.log(resp.message);
+        })
+    }
   }
 }
